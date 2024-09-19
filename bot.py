909d451e29a6d91fd76d8c3532c746ad61fbe27e
Niek Den Teuling <niekdt@users.noreply.github.com>
2024-09-19 22:20:19 +0200

    init bot

--- bot.py
+++ bot.py
@@ -6,14 +6,14 @@ from ...bot import Bot
 from ...linear_math import Transform
 
 
-class SimpleBot(Bot):
+class MinVerstappen(Bot):
     @property
     def name(self):
-        return "SimpleBot"
+        return "Min Verstappen"
 
     @property
     def contributor(self):
-        return "Nobleo"
+        return "Niek"
 
     def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
         target = self.track.lines[next_waypoint]
