154faf5aeeb452986634be242e1be49617d85fa0
Niek Den Teuling <niekdt@users.noreply.github.com>
2024-09-20 22:31:38 +0200

    cleanup code

--- bot.py
+++ bot.py
@@ -35,42 +35,32 @@ class MinVerstappen(Bot):
         return "Niek"
 
     def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
-        logging.info(f'NEXT WAYPOINT: {next_waypoint}')
-        pos = position.p
-
-        speed = velocity.length()
-        acc = speed - self.prev_speed  # t = 1
-        next_target = self.track.lines[next_waypoint]
-        target_distance = next_target.distance_to(pos)
-        logging.info(f'Speed: {speed:.2f}, acceleration: {acc:.2f}')
-
         def get_lines(offset=0, limit=20):
             start = (next_waypoint + offset) % len(self.track.lines)
             return list(islice(cycle(self.track.lines), start, start + limit))
 
+        pos = position.p
+        speed = velocity.length()
+        acc = speed - self.prev_speed  # t = 1
+        next_target = self.track.lines[next_waypoint]
         targets = get_lines()
+        target_distance = next_target.distance_to(pos)
+        target_angle = turn_angle(waypoint_angle(next_target, pos, targets[1]))
+
+        logging.info(f'NEXT WAYPOINT: {next_waypoint} (distance = {target_distance:.2f})')
+        logging.info(f'Speed: {speed:.2f}, acceleration: {acc:.2f}, waypoint angle: {target_angle:.2f}')
+
         wp_distances = np.array([x.distance_to(y) for (x, y) in pairwise([pos] + targets)])
         wp_cum_distances = wp_distances.cumsum()
 
         wp_angles = np.array([
-            turn_angle((prev_wp_pos - cur_wp_pos).angle_to(next_wp_pos - cur_wp_pos))
-            for prev_wp_pos, cur_wp_pos, next_wp_pos in
+            turn_angle(waypoint_angle(cur_pos, prev_pos, next_pos))
+            for prev_pos, cur_pos, next_pos in
             zip([pos] + get_lines(), get_lines(), get_lines(1))
         ])
-        logging.info(f'Waypoint angle: {wp_angles[0]:.2f}')
 
         wp_speeds = [max_corner_speed(a) for a in wp_angles]
-        max_speeds = [max_velocity(d, s) for d, s in zip(wp_cum_distances, wp_speeds)]
-
-        if any(wp_angles > 40):
-            sharp_wp = np.where(abs(wp_angles) > 40)[0][0]
-            sharp_wp_angle = wp_angles[sharp_wp]
-            sharp_wp_distance = wp_distances[:(sharp_wp + 1)].sum()
-            # logging.info(f'Sharp turn is {sharp_wp} waypoints away (distance {sharp_wp_distance:.2f})')
-        else:
-            sharp_wp_angle = 0
-            sharp_wp_distance = 0
-
+        max_speeds = [max_speed(d, s) for d, s in zip(wp_cum_distances, wp_speeds)]
 
         rel_target = position.inverse() * next_target
         angle = rel_target.as_polar()[1]
@@ -94,12 +84,16 @@ class MinVerstappen(Bot):
             return throttle, 0
 
 
+def waypoint_angle(pos, prev_pos, next_pos) -> float:
+    return (prev_pos - pos).angle_to(next_pos - pos)
+
+
 def turn_angle(angle: float) -> float:
     """ 0 = straight line, 180 = reverse"""
     return abs(180 - abs(angle))
 
 
-def max_velocity(distance: float, desire_speed: float) -> float:
+def max_speed(distance: float, desire_speed: float) -> float:
     return 60 * math.sqrt(
         (desire_speed / 60) ** 2 - 2 * MAX_DECELERATION / 60 * max(0.0, distance - 20)
     )
